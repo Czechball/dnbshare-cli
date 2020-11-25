4ac3a51c862cfe2f3a14143f27c080e5ebbe0399
Czechball <david@jenne.cz>
2020-11-25 16:54:39 +0100

    Added switches, automatic downloading using dnbshare-download, search

--- get-dnblist.sh
+++ get-dnblist.sh
@@ -1,4 +1,40 @@
-curl 'https://dnbshare.com/ajax/ajax_filelisting.php' \
+#!/bin/bash
+
+#Get file info url list from dnbshare.com
+
+DATE=$(date --iso-8601)
+
+while getopts ":s:*:" arg; do
+  case $arg in
+    s) QUERY="$OPTARG";;
+    *) SINGLE_OPTS="$OPTARG";;
+  esac
+done
+
+case $SINGLE_OPTS in
+    l)
+        MODE="latest=1"
+        NAME="Latest-$DATE";;
+    t)
+        MODE="trending=1"
+        NAME="Trending-$DATE";;
+    p)
+        MODE="popular=1"
+        NAME="Popular-$DATE";;
+    o)
+        MODE="top=1"
+        NAME="Top-$DATE";;
+esac
+
+if [[ $1 == "-s" ]]; then
+  MODE="filter=$QUERY"
+  NAME="Search-$QUERY-$DATE"
+
+fi
+
+echo "$NAME"
+
+PAGE_CONTENT=$(curl -s 'https://dnbshare.com/ajax/ajax_filelisting.php' \
   -H 'authority: dnbshare.com' \
   -H 'accept: text/html, */*; q=0.01' \
   -H 'x-requested-with: XMLHttpRequest' \
@@ -9,9 +45,15 @@ curl 'https://dnbshare.com/ajax/ajax_filelisting.php' \
   -H 'sec-fetch-mode: cors' \
   -H 'sec-fetch-dest: empty' \
   -H 'referer: https://dnbshare.com/download/' \
-  --data-raw 'latest=1'
-# latest: 'latest=1'
-# search: 'filter=string'
-# trending: 'trending=1'
-# popular: 'popular=1'
-# top: 'top=1'
+  --data-raw "$MODE")
+
+FILE_LIST=$(echo "$PAGE_CONTENT" | grep -Po 'class="file"><a href="\K[^"]+')
+PAGE_COMMENT=$(echo "$PAGE_CONTENT" | grep -Po '<!-- \K[^>]+')
+PAGE_INFO=${PAGE_COMMENT%--}
+
+echo "$PAGE_INFO"
+echo
+
+for FILE in $FILE_LIST; do
+  ./dnbshare-download.sh "https://dnbshare.com$FILE"
+done
