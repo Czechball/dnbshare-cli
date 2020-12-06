d4fd77b53e9eda6f969effe433d1d57e0b62a5bf
Czechball <david@jenne.cz>
2020-12-06 20:10:15 +0100

    improved argument handling

--- dnbshare-download.sh
+++ dnbshare-download.sh
@@ -9,30 +9,32 @@ fi
 
 DIRECTORY="$2"
 
-while getopts ":d:" arg; do
-  case $arg in
-    d) DIRECTORY="$OPTARG";;
-  esac
-done
-
-if [[ $* == "-h" ]]; then
-	echo "dnbshare-download by Czechball, part of dnbshare-cli"
+while getopts ":d:h" arg; do
+  case ${arg} in
+    d ) DIRECTORY="$OPTARG"
+	;;
+	h ) echo "dnbshare-download by Czechball, part of dnbshare-cli"
 	echo "https://github.com/Czechball/dnbshare-cli"
 	echo
-	printf "Usage:\n	%s <url>\nExample:\n	%s -d ~/Music https://dnbshare.com/download/Tsuki_Subsonic_-_Messiah_Madge_VIP_FREE_DOWNLOAD.mp3.html\n" "$0" "$0"
+	printf "Usage:\n	%s <url> (-d directory)\nExample:\n	%s -d ~/Music https://dnbshare.com/download/Tsuki_Subsonic_-_Messiah_Madge_VIP_FREE_DOWNLOAD.mp3.html\n" "$0" "$0"
 	echo
 	exit
-fi
+	;;
+	\? ) echo bruh; exit
+	;;
+  esac
+done
+shift $((OPTIND -1))
 
 URL="$1"
 PAGE_CONTENT=$(curl -s "$URL")
 DLFORM_FILENAME=$(echo "$PAGE_CONTENT" | grep -Po 'dlform-file" value="\K[^"]+')
 DLFORM_PAYLOAD=$(echo "$PAGE_CONTENT" | grep -Po 'dlform-payload" value="\K[^"]+')
 FILE_URL="$1?file=$DLFORM_FILENAME&payload=$DLFORM_PAYLOAD"
-mkdir -p "$DIRECTORY"
 
-if [[ DIRECTORY == "" ]]; then
+if [[ $DIRECTORY == "" ]]; then
 	wget --content-disposition -q --show-progress "$FILE_URL"
 else
+	mkdir -p "$DIRECTORY"
 	wget --content-disposition -q --show-progress -P "$DIRECTORY" "$FILE_URL"
 fi
